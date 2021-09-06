c8c9b68339bce7e47bcbcc26006c31f49f33c9f2
snowkiss13 <snowkiss13@gmail.com>
2021-09-07 07:24:42 +0900

    ログインページのエラーメッセージを作成

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'mimemagic'
+gem 'bcrypt'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence:true
-  validates :email, presence:true
+  validates :name, presence:true, length: { maximum: 30 }
+  validates :email, presence:true, length: { maximum: 255 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
