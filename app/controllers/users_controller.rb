096116bff30ebeec0640711246f251ccdac3b34a
snowkiss13 <snowkiss13@gmail.com>
2021-09-08 22:58:45 +0900

    sessionsを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,17 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    if @user.save
-      redirect_to user_path(@user.id)
-    else
-      render :new
-    end
-  end
   def new
     @user = User.new
   end
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :show ]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
