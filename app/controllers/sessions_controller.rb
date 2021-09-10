0deffbc1d817b9f541b4f2eabf727575e63fc488
snowkiss13 <snowkiss13@gmail.com>
2021-09-11 07:29:14 +0900

    バリデーション作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功した場合
+    else
+      # ログイン失敗した場合
+    end
+  end
 end
