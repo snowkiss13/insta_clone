0028a402b7cfa419cc5a9b6c0e544fe5194832a7
snowkiss13 <snowkiss13@gmail.com>
2021-09-14 08:54:38 +0900

    ログイン中のユーザを取得

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン成功した場合
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash[:danger] = 'ログインに失敗しました'
       render :new
