32c02c2d7876648cfdb9caedaa1735b0536f70e5
snowkiss13 <snowkiss13@gmail.com>
2021-10-02 10:52:11 +0900

    Feedsで画像アップロード機能を付加させるため作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_152032) do
+ActiveRecord::Schema.define(version: 2021_10_02_015035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
