23a6d095143bb500c77bc01aa83584c1e162e4d8
snowkiss13 <snowkiss13@gmail.com>
2022-03-17 22:40:49 +0900

    いじってるとちゅう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
+  def index
+    @userid = current_user.id
+    @users = User.all
+  end
   def new
     @user = User.new
   end
@@ -18,11 +22,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path([user.id])
   end
   private
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :feeds
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit,]
 end
