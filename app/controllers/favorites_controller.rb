f46506ab51fc3080446f26327f8b8238ccc7fcaa
snowkiss13 <snowkiss13@gmail.com>
2022-03-20 21:56:53 +0900

    Favoriteのアソシエーション

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,4 +7,7 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんのブログをお気に入り解除しました"
   end
+  def index
+    @favorites = current_user.favorites
+  end
 end
