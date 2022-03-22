7df8109b8744d28eea1c3f278b7d54fd86e49211
snowkiss13 <snowkiss13@gmail.com>
2022-03-22 22:08:17 +0900

    Bootstrapの導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require popper.js/dist/umd/popper.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
 
