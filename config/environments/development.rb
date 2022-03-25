16b538e1ec4a0ce1173752f2b5c7478aba324af7
snowkiss13 <snowkiss13@gmail.com>
2022-03-25 09:08:20 +0900

    Bug-fix:他人の投稿・プロフィールが編集できる問題を解消、エラーページに遷移できない問題を解消

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  # 404,500ページをテスト環境で有効にする場合→Falseにする
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
