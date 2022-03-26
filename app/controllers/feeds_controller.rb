416fd58f272e2a9dae97f02b7d4d447dc048c049
snowkiss13 <snowkiss13@gmail.com>
2022-03-27 00:06:39 +0900

    バリデーションの際の分岐を追加

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -46,7 +46,7 @@ class FeedsController < ApplicationController
 
   def confirm
     @feed = Feed.new(feed_params)
-    @feed = current_user.feeds.build(feed_params)
+    # @feed = current_user.feeds.build(feed_params)
     render :new if @feed.invalid?
   end
 
