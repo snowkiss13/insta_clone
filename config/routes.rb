d582be2765b99c0b197e038ec06c80f32067105c
snowkiss13 <snowkiss13@gmail.com>
2022-03-28 21:48:07 +0900

    if Rails.env.development?をぬく

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only: [:create, :destroy, :index]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # ↓試験環境では「if Rails.env.development?」を末尾に追加する
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
